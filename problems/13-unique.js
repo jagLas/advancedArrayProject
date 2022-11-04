1e5c61c9b5315a00de4393c1be1c50d03795286e
jagLas <jglasher@gmail.com>
2022-11-04 10:24:56 -0400

    final commit

--- problems/13-unique.js
+++ problems/13-unique.js
@@ -26,9 +26,9 @@ let unique = function(array) {
     return unique;
 };
 
-console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
-console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
-console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
+// console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
+// console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
+// console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- problems/16-hipsterfy.js
+++ problems/16-hipsterfy.js
@@ -37,9 +37,9 @@ let hipsterfy = function(sentence) {
     return words.join(' ');
 };
 
-console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
-console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
-console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+// console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+// console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+// console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
 
 // alternative solution using Array.map
 // let hipsterfy = function(sentence) {
